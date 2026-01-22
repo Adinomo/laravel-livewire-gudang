10f0756cdc3db9b66f8812a02fbc5b32f9713151
Adinomo <adinomo58@gmail.com>
2026-01-22 20:28:06 +0700

    mengubah navbar

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,45 @@
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+
+
+      <li class="nav-item dropdown user-menu">
+        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+          <img src="{{ asset ('adminlte3/dist/img/user2-160x160.jpg') }}" class="user-image img-circle " alt="User Image">
+          <span class="d-none d-md-inline">Adi Nomo Wibowo</span>
+        </a>
+        <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <!-- User image -->
+          <li class="user-header ">
+            <img src="{{ asset ('adminlte3/dist/img/user2-160x160.jpg') }}" class="img-circle " alt="User Image">
+
+            <p>
+              Adi Nomo 
+             <div>
+              <span class="badge badge-success">Admin</span>
+             </div>
+            </p>
+          </li>
+          <!-- Menu Body -->
+        
+          <!-- Menu Footer-->
+          <li class="user-footer">
+            <a href="#" class="btn btn-sm btn-danger float-right">
+              <i class="fas fa-sign-out-alt"></i>
+              log out</a>
+          </li>
+        </ul>
+      </li>
+     
+    </ul>
+  </nav>
+  <!-- /.navbar -->
---
+++ resources/views/superadmin/user/index.blade.php
@@ -0,0 +1,3 @@
+@extends('layouts.app')
+
+{{-- @livewire('superadmin.user.index') --}}
